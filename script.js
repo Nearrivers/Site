481af472bd3a14970cf2196bd6000c04525fa8fa
Fourcade Antoine <a.fourcade65@gmail.com>
2020-12-10 11:58:19 +0100

    appel du fichier json mit sur github alléger le fichier HTML
    Soucis d'objet JSON qui vient de nulle part ??

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 let carouselInner = $('.carousel.inner');
 let carouselIndicator = $('.carousel-indicator');
-let sources = $.getJSON('./img/carousel/sources.json');
+//let sources = $.getJSON('./img/carousel/sources.json');
 
-for (let source in sources) {
+function afficher(results) {
+    for (let result in results) {
 
-    let indicator = `<li data-target="#carouselExampleFade" data-slide-to="${source.number}"></li>`;
-    let item =  `<div class="carousel-item active" data-interval="2000">`
-                + ` <img src="${source.path}" class="d-block w-100" alt="..."></div>`;
+        console.log(result);
+        // console.log(results);
+        let indicator = `<li data-target="#carouselExampleFade" data-slide-to="${result.number}"></li>`;
+        let item =  `<div class="carousel-item active" data-interval="2000">`
+                    + ` <img src="${result.path}" class="d-block w-100" alt="..."></div>`;
 
-    carouselIndicator.append(indicator);
-    carouselInner.append(item);
-    
-    $('.carousel.active:first-child').addClass('active');
+        carouselIndicator.append(indicator);
+        carouselInner.append(item);
+        
+        $('.carousel.active:first-child').addClass('active');
+    }
 }
+
+fetch('https://raw.githubusercontent.com/Nearrivers/Site/master/img/carousel/sources.json')
+.then((response) => {
+    return response.json();
+}).then((res) => {
+    console.log(res);
+    afficher(res);
+})
+.catch((err) => {
+    console.log(err);
+})
